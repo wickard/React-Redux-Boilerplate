27b0a56666a076ff93568e63ac9e2512f114515f
Josh Carlson <carlson.joshuaph@gmail.com>
2018-01-27 13:26:13 -0600

    tutorial finished

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
-require('../../scss/style.scss');
+import UserList from '../containers/user-list'
+import UserDetail from '../containers/user-detail'
+require('../../scss/style.scss')
+
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
+  <div>
+    <h2>Username List:</h2>
+    <UserList />
+    <hr />
+    <h2>User Details:</h2>
+    <UserDetail />
+  </div>
 );
 
 export default App;
--- dev/js/index.js
+++ dev/js/index.js
@@ -1,19 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
+import {createStore} from 'redux';
+import allReducers from './reducers'
+import App from './components/app'
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
+const store = createStore(allReducers);
 
 ReactDOM.render(
     <Provider store={store}>
